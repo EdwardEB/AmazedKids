f87313f0e3a902976a8cd4b2d2c1d319ec73b365
Edward Engelbrecht <edward@bitechsystems.co.za>
2022-02-01 14:55:29 +0200

    changes to experiments

--- amazedkids/pages/products/index.tsx
+++ amazedkids/pages/products/index.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { GetStaticProps } from "next";
 import prisma from "../../lib/prisma";
+import { FixedSizeList } from 'react-window';
 // import Layout from "../components/Layout"
 // import Post, { PostProps } from "../components/Post"
 
 export const getStaticProps: GetStaticProps = async () => {
-  const feed = await prisma.products.findMany({
-    // where: { productname: "test2" },
+  // const res = await prisma.$executeRaw`select count(1) from product`
+  const feed = await prisma.product?.findMany({
+
+    where: { rid_product: {gt : 5000} },
   });
   return { props: { feed } };
 };
 
 const Products : React.FC<Props> = (props) => {
-  console.log(props)
+
+  const Row = ({ index, style }) => (
+    <div style={style}>
+      <div className="post">{props.feed[index].productname}</div>
+    </div>
+  );
+
   return (
-    <>
-      {props.feed.map((post) => (
-        <div key={post.id} className="post">{post.productname}</div>
-      ))}
-    </>
+    <FixedSizeList
+      height={500}
+      width={500}
+      itemSize={50}
+      itemCount={props.feed.length}
+    >
+      {Row}
+    </FixedSizeList>
   )
 }
 
