792a0dcfb8cc3d6d405fb07fb7045da0fc35aec0
Edward <edward@bitechsystems.co.za>
2022-02-03 20:16:27 +0200

    new experiments

--- amazedkids/pages/products.tsx
+++ amazedkids/pages/products.tsx
@@ -2,17 +2,19 @@ import { GetStaticProps } from "next";
 import React, { useEffect } from "react";
 import Layout from "../components/Layout";
 import prisma from "../lib/prisma";
-import {FixedSizeList} from "react-window";
+import { FixedSizeList } from "react-window";
+import { Card, Group, Text, Image, Badge, Button, Grid } from "@mantine/core";
+import { EZDiv } from "../components/EZDiv";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const feed = await prisma.product.findMany({where: { rid_product: {gt : 5000, lt : 7000}}})
+  const feed = await prisma.product.findMany({ where: { rid_product: { gt: 0, lt: 500 } } })
   return {
     props: { feed },
     revalidate: 60,
   }
 }
 
-const Products = (props )=> {
+const Products = (props) => {
 
   // useEffect(() => {
   //   fetch('api/product')
@@ -23,8 +25,28 @@ const Products = (props )=> {
   // }, [])
 
   const Row = ({ index, style }) => (
-    <div style={style}>
-      <div className="post">{props.feed[index].productname}</div>
+    <div>
+      <Card shadow="sm" padding="lg">
+        <Card.Section>
+          <Image src="boeti.JPG" height={160} alt="Norway" />
+        </Card.Section>
+
+        <Group position="apart" style={{ marginBottom: 5 }}>
+          <Text weight={500}>{props.feed[index].productname}</Text>
+          <Badge color="pink" variant="light">
+            On Sale
+          </Badge>
+        </Group>
+
+        <Text size="sm" style={{ color: "primary", lineHeight: 1.5 }}>
+          With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+          activities on and around the fjords of Norway
+        </Text>
+
+        <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
+          Book classic tour now
+        </Button>
+      </Card>
     </div>
   );
 
@@ -32,9 +54,10 @@ const Products = (props )=> {
     <Layout>
       <FixedSizeList
         height={500}
-        width={500}
+        width={"100%"}
         itemSize={50}
         itemCount={props.feed.length}
+        display={"flex"}
       >
         {Row}
       </FixedSizeList>
